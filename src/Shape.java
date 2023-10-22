4f5c826d85fe0e98e714f8d7a19c6a9579305cb1
alexsavas18 <alexsavas18@gmail.com>
2023-10-22 17:11:07 -0500

    Create Shape.java

---
+++ src/Shape.java
@@ -0,0 +1,10 @@
+/**************************************************************************************************
+ *  Description: Assignment for my Object-Oriented Programming course that tested abstract classes
+ *  and methods, as well as inheritance
+ *  - This is the Abstract Shape class that includes a header for the getArea() function, which will
+ *  return the area of a specific shape
+ *************************************************************************************************/
+public abstract class Shape
+{
+    public abstract double getArea();
+}
